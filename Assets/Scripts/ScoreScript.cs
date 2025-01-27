82f994fbedae530d37683341564c5921823dfdee
Oliver <OFBrochesterkent@gmail.com>
2025-01-27 10:10:55 +0000

    changes

---
+++ Assets/Scripts/ScoreScript.cs
@@ -0,0 +1,38 @@
+using System.ComponentModel.Design.Serialization;
+using TMPro;
+using UnityEngine;
+
+public class ScoreScript : MonoBehaviour
+{
+    public static ScoreScript instance;
+    private int score;
+    public TextMeshProUGUI ScoreText;
+
+    private void Start()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(this);
+        }
+        else
+        {
+            instance = this;
+        }
+    }
+    public void IncrementScore(int increase)
+    {
+        score = score + increase;
+        UpdateScoreDisplay();
+    }
+
+    public void UpdateScoreDisplay()
+    {
+        ScoreText.text = "Score: " + score;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
